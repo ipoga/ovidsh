482167849ef9b0b9a5dfc04237471be49e5c54bc
Jens Peter Andersen <ip.andersen@gmail.com>
2014-10-08 14:19:35 +0200

    Testing new display.

--- javascripts/bookmarklet.js
+++ javascripts/bookmarklet.js
@@ -16,7 +16,7 @@ if (h == null) {
     h = document.createElement("div");
     document.body.appendChild(h);
 } else {
-    h.style.display = "block";
+    h.style.display = "initial";
     console.log("Div exists");
 }
 h.innerHTML = "<div style='width: 100%;background: #eee;position: relative;padding: 20px; border: 1px solid #777;resize:both;overflow: scroll;z-index:999' id='ovidsh_display'>OvidSH (<a href='javascript:var h = document.getElementById(\"ovidsh_display\");h.style.display = \"none\";void(0);'>Close this window</a>)<hr/><pre>" + out1 + "<br/><br/>" + out2 + "</pre></div>";
