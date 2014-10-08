34e8f7e220be09e92e8333c0f72a7fe08692ddf4
Jens Peter Andersen <ip.andersen@gmail.com>
2014-10-08 13:40:35 +0200

    Testing new display.

--- javascripts/bookmarklet.js
+++ javascripts/bookmarklet.js
@@ -11,6 +11,12 @@ for (var i = 0; i < ids.length - 1; i++) {
     out1 = out1 + id + "\t" + term + "\t" + hit + "\n";
     out2 = out2 + term + "\n";
 }
-var h = document.createElement("div");
-document.body.appendChild(h);
-h.innerHTML = "<div width=200 height=300 style='z-index:1;position: absolute; top: 20px;left: 20px; padding:20px;'><pre>" + out1 + "<br/><br/>" + out2 + "</pre></div>";
+var h = document.getElementById("ovidsh_display");
+if (h == null) {
+    console.log("Creating div");
+    h = document.createElement("div");
+    document.body.appendChild(h);
+} else {
+    console.log("Div exists");
+}
+h.innerHTML = "<div width='200' height='300' style='background: #ccc' id='ovidsh_display'><pre>" + out1 + "<br/><br/>" + out2 + "</pre></div>";
