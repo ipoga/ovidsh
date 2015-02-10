7d5c4c6326077fcd21bd9a65d6f9756eee5e0372
Jens Peter Andersen <ip.andersen@gmail.com>
2015-02-10 16:32:10 +0100

    Added script to hta.js

--- javascripts/hta.js
+++ javascripts/hta.js
@@ -1,7 +1,51 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * This script takes a newline-separated sctring of HTA-database style
+ * references and cconverts them into standard RIS style format.
+ * @param hta the original data as string
+ * @return  the RIS formatted string
  */
-
-
+function hta(hta) {
+    var ls = split("\n", hta);
+    var l;
+    for (i = 0; i < length(ls); i++) {
+        l = ls[i];
+        if (substr(l, 0, 6) == "Record") {
+            l = "TY  - RPRT";
+            i++;
+        } else {
+            var d = substr(l, 0, 3);
+            var f = trim(substr(l, 4));
+            switch (d) {
+                case "TTL":
+                    l = "T1  - $f";
+                    break;
+                case "PUB":
+                    l = "PB  - $f";
+                    break;
+                case "XYR":
+                    l = "Y1  - $f";
+                    break;
+                case "RUR":
+                    l = "ER  - \n";
+                    break;
+                case "AUT":
+                    var aus = split(",", f);
+                    if (length(aus) > 1) {
+                        l = "";
+                        for (j = 0; j < aus.length; j++) {
+                            var au = trim(aus[j]);
+                            l = l + "A1  - " + au + "\n";
+                        }
+                        l = substr(l, 0, -1);
+                    } else {
+                        l = "A1  - $f";
+                    }
+                    break;
+                default:
+                    l = "";
+                    break;
+            }
+        }
+    }
+    return l;
+}
