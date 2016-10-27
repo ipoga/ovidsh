c06797649d3c381fd64d88e2662d1a28b0f871e6
au140354 <au140354@d18740.uni.au.dk>
2016-10-27 11:53:39 +0200

    Removing test console logs.

--- javascripts/bookmarklet.js
+++ javascripts/bookmarklet.js
@@ -8,19 +8,16 @@ for (var i = 0; i < ids.length - 1; i++) {
     var term = terms[i].innerHTML.trim();
     var hit = hits[i + 1].innerHTML.replace(/<(?:.|\n)*?>/gm, '');
     hit = parseInt(hit);
-    console.log(hit);
     out1 = out1 + id + "\t" + term + "\t" + hit + "\n";
     out2 = out2 + term + "\n";
 }
 var h = document.getElementById("ovidsh_display");
 if (h == null) {
-    console.log("Creating div");
     h = document.createElement("div");
     var first = document.body.firstChild;
     document.body.insertBefore(h,first);
     //document.body.appendChild(h);
 } else {
     h.style.display = "initial";
-    console.log("Div exists");
 }
 h.innerHTML = "<div style='width: 100%;background: #fff;position: relative;padding: 20px; border-bottom: 1px solid #777;' id='ovidsh_display'>OvidSH (<a href='javascript:var h = document.getElementById(\"ovidsh_display\");h.style.display = \"none\";void(0);'>Close this window</a>)<hr/><pre>" + out1 + "<br/><br/>" + out2 + "</pre></div>";
