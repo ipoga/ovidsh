237cb45d4607985f6a3777f6ea171d5537f71ad3
au140354 <au140354@d18740.uni.au.dk>
2016-10-27 11:27:55 +0200

    testing

--- javascripts/bookmarklet.js
+++ javascripts/bookmarklet.js
@@ -7,7 +7,7 @@ for (var i = 0; i < ids.length - 1; i++) {
     var id = ids[i + 1].innerHTML;
     var term = terms[i].innerHTML.trim();
     var hit = hits[i + 1].innerHTML;
-    out1 = out1 + id + "\t" + term + "\t" + hit + "\n";
+    out1 = out1 + id + "\t" + "[("+term + ")\t]" + hit + "\n";
     out2 = out2 + term + "\n";
 }
 var h = document.getElementById("ovidsh_display");
